6eb49bcaf74d8104b4b91bcf6736adf8ef6d11ba
Philippe Odoubadé <philippejide@outlook.com>
2019-12-14 06:33:49 +0100

    implemented redux authentication with localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import {setCurrentUser} from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store'
 
@@ -13,7 +16,16 @@ import Login from './components/auth/Login';
 import './App.css';
 import { format } from 'path';
 
+// Check for token
 
+if(localStorage.jwtToken) {
+  // set auth token header to auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 function App() {
   return (
