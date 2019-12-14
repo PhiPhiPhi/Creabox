42e89314d45f204fc9d45e001a0e3b671796a3b2
Philippe Odoubadé <philippejide@outlook.com>
2019-12-14 08:08:26 +0100

    implement conditional navbar and automatic redirection

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import {logoutUser} from './actions/authActions';
 import './App.css';
 import { format } from 'path';
 
@@ -25,6 +26,15 @@ if(localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now()/1000;
+  if(decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+    //TODO: Clear current profile
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 function App() {
@@ -43,6 +53,8 @@ function App() {
     </Router>
     </Provider>
   );
-}
+};
+
+
 
 export default App;
